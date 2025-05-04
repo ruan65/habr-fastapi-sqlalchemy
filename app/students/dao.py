c50be2106dac0e3e393aa807a324521bcc30939c
ruan65 <ruan65@gmail.com>
2025-05-04 14:36:05 +0300

    fix duplicates

--- app/students/dao.py
+++ app/students/dao.py
@@ -2,9 +2,24 @@ from app.students.models import Student, Major
 from app.dao.base import BaseDAO
 from app.database import async_session_maker
 from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 
 class StudentDAO(BaseDAO):
     model = Student
+
+    @classmethod
+    async def find_all(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model).options(joinedload(cls.model.major)).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.scalars().all()
+
+    @classmethod
+    async def find_one_or_none(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model).options(joinedload(cls.model.major)).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.scalar_one_or_none()
 
     @classmethod
     async def find_full_data(cls, student_id: int):
@@ -27,33 +42,3 @@ class StudentDAO(BaseDAO):
             student_data['major'] = major_info.major_name
 
             return student_data
-
-
-from sqlalchemy import select
-from sqlalchemy.orm import joinedload
-from app.dao.base import BaseDAO
-from app.students.models import Student
-
-class StudentDAO(BaseDAO):
-    model = Student
-
-    @classmethod
-    async def find_all(cls, **filter_by):
-        async with async_session_maker() as session:
-            query = select(cls.model).options(joinedload(cls.model.major)).filter_by(**filter_by)
-            result = await session.execute(query)
-            return result.scalars().all()
-
-    @classmethod
-    async def find_one_or_none(cls, **filter_by):
-        async with async_session_maker() as session:
-            query = select(cls.model).options(joinedload(cls.model.major)).filter_by(**filter_by)
-            result = await session.execute(query)
-            return result.scalar_one_or_none()
-
-    @classmethod
-    async def find_full_data(cls, student_id: int):
-        async with async_session_maker() as session:
-            query = select(cls.model).options(joinedload(cls.model.major)).filter_by(id=student_id)
-            result = await session.execute(query)
-            return result.scalar_one_or_none()
