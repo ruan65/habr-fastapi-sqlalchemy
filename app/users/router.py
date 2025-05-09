5dafe0fb1fda4a19c1aade110f641f20c32e9175
arudenko <ruan65@gmail.com>
2025-05-09 12:54:18 +0300

    register-auth-user

---
+++ app/users/router.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, HTTPException, status, Response
+from app.users.auth import get_password_hash, create_access_token, authenticate_user
+from app.users.dao import UsersDAO
+from app.users.schemas import SUserRegister, SUserAuth
+
+
+router = APIRouter(prefix='/auth', tags=['Auth'])
+
+
+@router.post("/register/")
+async def register_user(user_data: SUserRegister) -> dict:
+    user = await UsersDAO.find_one_or_none(email=user_data.email)
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail='Пользователь уже существует'
+        )
+    user_dict = user_data.dict()
+    user_dict['password'] = get_password_hash(user_data.password)
+    await UsersDAO.add(**user_dict)
+    return {'message': 'Вы успешно зарегистрированы!'}
+
+
+@router.post("/login/")
+async def auth_user(response: Response, user_data: SUserAuth):
+    check = await authenticate_user(email=user_data.email, password=user_data.password)
+    if check is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Неверная почта или пароль')
+    access_token = create_access_token({"sub": str(check.id)})
+    response.set_cookie(key="users_access_token", value=access_token, httponly=True)
+    return {'access_token': access_token, 'refresh_token': None}
