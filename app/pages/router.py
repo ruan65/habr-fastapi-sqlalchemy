e1ae79dbce0a00d71996dca3f1c783c3b72c2bfa
arudenko <ruan65@gmail.com>
2025-05-11 21:38:38 +0300

    login and logout

--- app/pages/router.py
+++ app/pages/router.py
@@ -1,20 +1,31 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
-from app.students.router import get_all_students_for_front
-from app.students.router import get_student_by_id
-
+from app.students.router import get_all_students_for_front, get_student_by_id
+from app.users.router import get_me
 
 router = APIRouter(prefix="/pages", tags=["Фронтенд"])
 templates = Jinja2Templates(directory="app/templates")
 
-@router.get('/register')
+
+@router.get("/register")
 async def get_students_html(request: Request):
-    return templates.TemplateResponse(name='register_form.html', context={'request': request})
+    return templates.TemplateResponse(
+        name="register_form.html", context={"request": request}
+    )
 
 
-@router.get('/login')
+@router.get("/login")
 async def get_students_html(request: Request):
-    return templates.TemplateResponse(name='login_form.html', context={'request': request})
+    return templates.TemplateResponse(
+        name="login_form.html", context={"request": request}
+    )
+
+
+@router.get("/profile")
+async def get_my_profile(request: Request, profile=Depends(get_me)):
+    return templates.TemplateResponse(
+        name="profile.html", context={"request": request, "profile": profile}
+    )
 
 
 @router.get("/students")
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -96,3 +96,27 @@ async function loginFunction(event) {
         alert('Произошла ошибка при входе. Пожалуйста, попробуйте снова.');
     }
 }
+
+async function logoutFunction() {
+    try {
+        // Отправка POST-запроса для удаления куки на сервере
+        let response = await fetch('/auth/logout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        // Проверка ответа сервера
+        if (response.ok) {
+            // Перенаправляем пользователя на страницу логина
+            window.location.href = '/pages/login';
+        } else {
+            // Чтение возможного сообщения об ошибке от сервера
+            const errorData = await response.json();
+            console.error('Ошибка при выходе:', errorData.message || response.statusText);
+        }
+    } catch (error) {
+        console.error('Ошибка сети', error);
+    }
+}
